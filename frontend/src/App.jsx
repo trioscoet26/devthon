229e414ddb169963cf4393348ecdc9f85e2d1763
unknown <Mithuleshwatane11@gmail.com>
2025-03-17 22:25:23 +0530

    mith

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,6 +14,7 @@ import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
 import AdminNavbar from "./adminnavbar";
 import React, { useState , useEffect } from "react";
+import Chatbot from './Chatbot'; // Make sure this path is correct
 
 
 function App() {
@@ -45,6 +46,8 @@ useEffect(() => {
         <Route path="/admin-dashboard" element={<AdminDashboard />} />
          
       </Routes>
+      <Chatbot />
+
       <Footer />
     <ToastContainer />
     </Router>
